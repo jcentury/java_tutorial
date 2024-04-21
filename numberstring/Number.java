aa378cfa05ab243d89e4f762524cc38d1c6004de
jcentury <tprl0328@naver.com>
2024-04-22 01:51:23 +0900

    number

---
+++ numberstring/Number.java
@@ -0,0 +1,16 @@
+package numberstring;
+
+public class Number {
+
+    public static void main(String[]args){
+        //더하기
+        System.out.println(1+2);
+        //실수
+        System.out.println(1.2+1.3);
+        //곱하기
+        System.out.println(2*5);
+        //나누기
+        System.out.println(6/2);
+    }
+    
+}
